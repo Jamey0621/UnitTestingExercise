5ea954939f2bd99ea69f2115ce34a9f649878589
Jamey Geoghagan <Jameygeoghagan@gmail.com>
2020-07-14 18:10:05 -0500

    Was fun learnign about unit testing

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,77 +1,92 @@
 using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1,2,3,6)]
+        [InlineData(1,5,6,12)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var num = new UnitTestMethods();
             //Act
+            var actual = num.AddNum(num1, num2, num3);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(2,1,1)]
+        [InlineData(5,4,1)]
+        [InlineData(1000,999,1)]
+        public void Sub(int minuend, int subtrhend, int expected)
+        {
+            //Arrange
+            var num = new UnitTestMethods();
+            //Act
+            var act = num.Sub(minuend, subtrhend);
 
             //Assert
+            Assert.Equal(expected, act);
+        }
+
+        [Theory]
+        [InlineData(5,5,25)]
+        [InlineData(25,5,100)]
+        [InlineData(5,20,100)]
+        public void Mult(int num1, int num2, int expected)
+        {
+            //Arrange
+            var num = new UnitTestMethods();
+            //Act
+            var act = num.Mult(num1, num2);
+            //Assert
+            Assert.Equal(expected, act);
 
         }
 
         [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
+        [InlineData(4,2,2)]
+        [InlineData(6,3,2)]
+        [InlineData(100,50,2)]
+        public void Div(int num1, int num2, int expected)
         {
             //Arrange
-
+            var num = new UnitTestMethods();
             //Act
-
+            var act = num.Div(num1, num2);
             //Assert
-
-        }
-
-        [Theory]
-        [InlineData()]
-        public void Multiply(int num1, int num2, int expected)
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-
-        }
-
-        [Theory]
-        [InlineData()]
-        public void Divide(int num1, int num2, int expected)
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-
+            Assert.Equal(expected, act);
         }
 
         [Fact]
-        public void YourMethodName()
+        public void Add3And3()
         {
             //Arrange
-
+            var num = new UnitTestMethods();
             //Act
 
-            //Assert
+            var actual = num.Add3And3();
+            var expected = 6;
 
+            //Assert
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void GetHey()
         {
             //Arrange
-
+            var word = new UnitTestMethods();
             //Act
-
+            var act = word.GetHey();
+            var expt = "Hey!";
             //Assert
+            Assert.Equal(expt, act);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -1,26 +1,65 @@
 ﻿using System;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+
 namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+            // Righ-click on Dependencies in your .Tests project and set your reference // DONE
 
 
 
-        // Create an Add method that passes 3 integers
+        // Create an Add method that passes 3 integers// done
+
+        public int AddNum(int a, int b, int c )
+        {
+            int sum = 0;
+            sum = a +  b + c;
+            return sum;
+        }
 
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
+         public int Sub(int minuend, int subtrahend)
+        {
+            int ans = 0;
+            ans = minuend - subtrahend;
+            return ans;
+             
+        }
 
         // Create a Multiply method that passes 2 integers
+        public int Mult(int a, int b)
+        {
+            int ans = 0;
+            ans = a * b;
+            return ans;
+        }
+        // Create a Divide method that passes 2 integers// Done
 
-        // Create a Divide method that passes 2 integers
-
-
+        public int Div(int num1, int num2)
+        {
+            int ans = 0;
+            ans = num1 / num2;
+            return ans;
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        
+        //Amethod that returns 6
+        public int Add3And3()
+        {
+            int ans = 0;
+            ans = 3+ 3;
+            return ans;
+        }
 
+        public string GetHey()
+        {
+            return "Hey!";
+        }
         
     }
 }
